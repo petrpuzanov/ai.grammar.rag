cb488c906406f8ff7ff1d721a826bfb0d6793f2c
Petr Puzanov <peta12321@gmail.com>
2026-04-10 10:59:26 +0900

    init commit

---
+++ src/test/java/org/petrpuzanov/ai/grammar/rag/ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.petrpuzanov.ai.grammar.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
